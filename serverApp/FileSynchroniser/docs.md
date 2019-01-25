00b3925792886876e8f157576f2f9789be932dd3
= <=>
2019-01-25 23:26:31 +0000

    Delete in push function now check version to avoid conflicts and docs for push are finished

--- serverApp/FileSynchroniser/docs.md
+++ serverApp/FileSynchroniser/docs.md
@@ -2,17 +2,24 @@
 ***
 ## 1. Push file changes
 
+### Description
 This function is used by the clients when they want to save the changes made to a file in their local machine. this includes creation of new files, modification of existing files and deletion of existing files. In the case of modification or deletion of the file, the file must be in the last version. If it is not the last version it will return an error indicationg a conflict.
 
-### Request:
+### Request
 * Method: POST 
-* Parameter: 
+* Parameters: 
 
-| Command | Description |
-| --- | --- |
-| `git status` | List all *new or modified* files |
-| `git diff` | Show file differences that **haven't been** staged |
+    | Parameter     | Description           | Type  | Values    |
+    | ---           | ---                   | ---   | ---       |
+    | `file`        | File the client wants to upload | File | File |
+    | `version`     | Current version of the file used to check for conflicts | Text | Version number |
+    | `delete`      | If `file` was not specified it indicates delete mode | Text | {"true", "false"} (if "true" `file` must not be passed) |
+    | `filename`    | The name of the file to be deleted id `delete` is `true` | Text | Filename |
 
+### How it works
+If `file` is passed the filename of that file will be checked to see if there is information about the file in the database. If there is no file information stored with that filename the file will be saved in the directory and the file information saved in the database and the server will respond with a success message including the information of the file. If there is a file with that filename, a hash of the file will be calculated and compared with the one stored in the database to see if the file has changed. If the file has not changed the server will respond with an error stating that the file has not been modified. On the other hand if the file has changed the `version` will be checked to see if the file was the last version. If it is not the last version this means that the file has changed since the user downloaded it and therefore updating it would create a conflict in the file. In this case the server will respond with an error indicating there is a version conflict. If the `version` was the last version then the file will be updated in the directory and the information updated in the database.
+
+If `file` was not passed the `delete` parameter will be checked to see if the client wants to delete a file. If it not equal to "true" the server will respond with an error indicating that the user did not include `file`. If `delete` is "true" the server will check if there is information of a file with filename equal to `filename` and if `version` is the last version. If there is information will be deleted from the database and the file deleted from the directory. A response will be sent indicating if the file was deleted successfully. 
 
 ***
 ## 2.
--- serverApp/FileSynchroniser/src/routes/files.js
+++ serverApp/FileSynchroniser/src/routes/files.js
@@ -23,7 +23,6 @@ router.post('/push', (req, res)=> {
         if (req.files.file) {
             let userFile = req.files.file;            
             //Look in DB to see if file already exists
-            //TODO: check if it exists on the file system (DB and FS are out of sync)
             File.findOne({ filename: userFile.name }, (err, file)=>{
                 if (err) {
                     console.error(err);
@@ -47,7 +46,7 @@ router.post('/push', (req, res)=> {
                                 fs.mkdirSync(global.fileDirectory, { recursive: true });
                             }
                             //Move file to the directory
-                            userFile.mv(path.join(global.fileDirectory, userFile.name), (err)=> {
+                            userFile.mv(path.join(file.path), (err)=> {
                                 if (err) {
                                     console.error(err);
                                     return res.status(500).send({success: false, error:'Error saving the file'});
@@ -114,7 +113,6 @@ router.post('/push', (req, res)=> {
         //Check if delete flag is true
         if( req.body.delete === "true" ){ //delete == true
             //Look in the DB to see if file exists
-            //TODO: check if it exists on the file system (DB and FS are out of sync)
             File.findOne({ filename: req.body.filename }, (err, file)=>{
                 if (err) {
                     console.error(err);
@@ -122,23 +120,28 @@ router.post('/push', (req, res)=> {
                 }
                 //Check that file exists
                 if(file){
-                    //Delete/unlink the file
-                    fs.unlink(file.path, (err) => {
-                        if (err) {
-                            console.error(err);
-                            return res.status(500).send({success: false, error: "Error deleting file"});
-                        }
-                        //Delete file info from DB
-                        File.deleteMany({ filename: req.body.filename }, function (err) {
+                    //Check if file is last version
+                    if(parseInt(req.body.version) === file.version){ //Files versions are the same so there is no conflict
+                        //Delete/unlink the file
+                        fs.unlink(file.path, (err) => {
                             if (err) {
-                                //TODO: should resave the file to the dir
                                 console.error(err);
-                                return res.status(500).send({success: false, error: "Error deleting file info from database"});
+                                return res.status(500).send({success: false, error: "Error deleting file"});
                             }
-                            //File deleted successfully
-                            return res.send({success: true, file: null});
+                            //Delete file info from DB
+                            File.deleteMany({ filename: req.body.filename }, function (err) {
+                                if (err) {
+                                    //TODO: should resave the file to the dir
+                                    console.error(err);
+                                    return res.status(500).send({success: false, error: "Error deleting file info from database"});
+                                }
+                                //File deleted successfully
+                                return res.send({success: true, file: null});
+                            });
                         });
-                    });
+                    }else{ //File versions are different HTTP 409 == Conflict
+                        res.status(409).send({success: false, error: "The file was not up to date, not deleting it to avoid conflicts"})
+                    }
                 }else{ //file does not exist send error
                     return res.status(400).send({success: false, error:'File to delete does not exist'});
                 }
