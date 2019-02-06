8116e8e2811b6b5416abf69923be83e409f9ca60
Nikolaos Rizos <nikolaos.rizos@kcl.ac.uk>
2019-02-06 16:02:54 +0000

    comment devtools

--- desktopApp/FileSynchroniser/index.js
+++ desktopApp/FileSynchroniser/index.js
@@ -34,7 +34,7 @@ function boot() {
 		}
 	})
 	win.loadURL(`file://${__dirname}/index.html`)
-	win.webContents.openDevTools();
+	// win.webContents.openDevTools();
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 var menu = Menu.buildFromTemplate([
