b2719d06534fe0536b9ff021cdce37abc7a581ed
Alain Doebeli <alain.doebeli@kcl.ac.uk>
2019-02-25 02:40:12 +0000

    Initial final report template

--- desktopApp/FileSynchroniser/config/configuration.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    getServerFilesURL: 'http://18.130.64.155/file/getInfo',
-    syncFileURL: 'http://18.130.64.155/file/push', 
-    downloadFileURL: 'http://18.130.64.155/file/getFile',
-    getDiffURL: 'http://18.130.64.155/file/getDiff'
-}
